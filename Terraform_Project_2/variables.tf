a859d915016e98b4bcc8a9854d519e183b8f1f0b
Tejaswini <tejaswinibojja@gmail.com>
2026-04-24 01:00:58 +0530

    This is to commit & publish dev_tejaswini branch

--- Terraform_Project_2/variables.tf
+++ Terraform_Project_2/variables.tf
@@ -10,3 +10,17 @@ variable "instance_type" {
     type = string 
 }
 
+variable "vpc_cidr" {
+    type = string
+}
+variable "public_subnet_cidr" {
+    type = string
+}
+
+variable "private_subnet_cidr" {
+    type = string
+}
+
+variable "key_pair_path" {
+   type  = string
+}
